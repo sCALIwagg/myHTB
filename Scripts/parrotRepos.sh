c74a8fe96c21d6482c22101c606963b0395b070a
Austin M <69631945+sCALIwagg@users.noreply.github.com>
2025-05-21 10:53:58 -0700

    added a conditional to test my skills in bash

--- Scripts/parrotRepos.sh
+++ Scripts/parrotRepos.sh
@@ -9,4 +9,12 @@ echo "Hello, friend!"
 echo "What distro are you using? (more will be added later)"
 read -p "1=Ubuntu, 2=Debian: (int pls)" distro
 
-echo $distro
+if [$distro -eq 1];
+then
+    echo "You're using Ubuntu"
+elif [$distro -z || $distro -eq 0];
+then
+    echo "Please enter an integer"
+else
+    echo "No support for other distros atp"
+fi
